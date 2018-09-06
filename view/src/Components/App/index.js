605d424bdade80cb8d3335a869084c3beb1a1bf9
halimeozturk <ozturkk.halimee@gmail.com>
2018-09-06 12:19:47 +0300

    timer added

--- view/src/Components/App/index.js
+++ view/src/Components/App/index.js
@@ -5,9 +5,9 @@ const API = 'http://localhost:8000/';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       data: [],
+      count: 0,
     };
   }
 
@@ -20,8 +20,24 @@ class App extends Component {
           data : _data
         })
       )
+
+    this._interval = setInterval(() => {
+      this.setState({count: this.state.count + 1});
+      
+      fetch(API)
+      .then(response => response.json())
+      .then(_data => 
+        this.setState({
+          data : _data
+        })
+      )
+    }, 5000);
+
     }
   
+  componentWillUnmount() {
+    clearInterval(this._interval);
+}
 
   render() {
     return (
@@ -38,23 +54,20 @@ class App extends Component {
            </tr>
            {this.state.data.map((item, i) => (
              <tr key={i}>
-               <td width="1000px"><li><div>{item.name}</div></li></td> 
-               <td width="1000px">{item.market_cap}</td>
-               <td width="1000px">{item.price}</td>
-               <td width="1000px">{item.percent_change_24h}</td>
-               <td width="1000px">{item.percent_change_24h}</td>
-               <td width="1000px">{item.percent_change_7d}</td> 
-               </tr>
+              <td width="1000px"><li>{item.name}</li></td> 
+              <td width="1000px">{item.market_cap}</td>
+              <td width="1000px">{item.price}</td>
+              <td width="1000px">{item.percent_change_24h}</td>
+              <td width="1000px">{item.percent_change_24h}</td>
+              <td width="1000px">{item.percent_change_7d}</td> 
+              </tr>
            ))}
            </tbody>
-        </table>
-         
+        </table>  
        </div>
     );
   }
 
 }
-
-
 
 export default App;
